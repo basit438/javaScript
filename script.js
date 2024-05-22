6e6467aaeedaad9f57831083f4f9fb4cf2fd8f09
Basit <basitmanzoor438@gmail.com>
2024-05-22 13:58:16 +0530

    uploading dom assignment1

---
+++ script.js
@@ -0,0 +1,8 @@
+const box = document.getElementById('mydiv');
+box.style.backgroundColor = "yellow";
+box.style.margin ="20px";
+box.style.padding ="10px";
+box.style.fontSize = "18px";
+box.style.fontWeight= "bold";
+box.style.height ="200px";
+box.style.width ="300px";
